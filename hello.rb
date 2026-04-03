1ccfdd47814e848aaed469854ee93f71e8e1fd2f
engrfarazhashmikhi <farazhashmi90000@gmail.com>
2026-04-03 16:48:53 +0000

    Ruby Programming Has Been STARTED!

---
+++ hello.rb
@@ -0,0 +1,9 @@
+greetings = "Assalam-O-Alaikum"
+userName = nil
+
+puts "Enter your name please: ";
+userName = gets.chomp()
+
+puts(greetings + " " + userName)
+puts("ENJOY coding in Ruby Programming Language.")
+
