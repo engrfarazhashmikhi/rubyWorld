851a8c47eb587bbb87a67c4ff79ad1fd08c4e336
engrfarazhashmikhi <farazhashmi90000@gmail.com>
2026-04-05 18:36:50 +0000

    Ruby For Loop + Ruby Classes And Ruby Objects Example

--- loops.rb
+++ loops.rb
@@ -1,22 +1,45 @@
-def mathTable(num = 0)
-  getTableNum = num.to_i
+class Loops
+  def mathTable(num = 0)
+    getTableNum = num.to_i
   
-  counter = 1
+    counter = 1
  
-  puts "Table of #{getTableNum}"
-  puts "======================="
-  while counter <= 12
-    puts "#{getTableNum} * #{counter} = #{(getTableNum * counter)}"
-    counter += 1
+    puts "Table of #{getTableNum}"
+    puts "======================="
+    while counter <= 12
+      puts "#{getTableNum} * #{counter} = #{(getTableNum * counter)}"
+      counter += 1
+    end
+
   end
 
+  def play()
+    puts "Enter any random NUMBER: "
+    table_number = gets.chomp().to_i
+    mathTable(table_number)
+  end
+
+  def forLoop()
+
+    counter = 1..10
+
+    for c in counter
+
+      puts "#{c}"
+
+    end
+
+    puts ""
+
+    fruits = ["Apples", "Strawberries", "Pineapples"]
+
+   fruits.each do |fruit|
+      puts "We have #{fruit.to_s}"
+   end
+  end
 end
 
-def play()
-  puts "Enter any random NUMBER: "
-  table_number = gets.chomp().to_i
-  mathTable(table_number)
-end
+loop = Loops.new()
 
-play()
+loop.forLoop()
 
