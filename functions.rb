76492f6cfcb34d052881ac27483a669cd2b3ef70
engrfarazhashmikhi <farazhashmi90000@gmail.com>
2026-04-12 16:15:16 +0000

    Tax Applied to Bill and Subtotal return at checkout.

--- functions.rb
+++ functions.rb
@@ -14,7 +14,9 @@ module Functions
     standard_delivery = self.getInputFromUser("Enter delivery charges: ")
     discount_applied = self.getInputFromUser("Enter discount applied: ")
     platform_fee = self.getInputFromUser("Enter Extra Charges: ")
+    tax_applied = (15/100).to_f
     subtotal = ((order_price.to_f + standard_delivery.to_f + platform_fee.to_f)-discount_applied.to_f)
+    subtotal += tax_applied
     return subtotal.to_f
   end
 
@@ -22,7 +24,7 @@ module Functions
     getBillPrice = self.generateBill().to_f
     payment = self.getInputFromUser("Payment Please: ").to_f
     change = (payment - getBillPrice)
-    return { "Payment" => payment, "Your Change: " => change } 
+    return { "Payment" => payment, "Your Change: " => change, "Subtotal" => getBillPrice}
   end
 end
 
