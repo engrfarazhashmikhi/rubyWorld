4b5baa9bc7390d27bb952006b8706cad97474e9e
engrfarazhashmikhi <farazhashmi90000@gmail.com>
2026-04-19 16:24:58 +0000

    Tax Applied To Order Price.

--- functions.rb
+++ functions.rb
@@ -10,13 +10,12 @@ module Functions
   end
 
   def self.generateBill
-    order_price = self.getInputFromUser("Enter Order Price: ")
-    standard_delivery = self.getInputFromUser("Enter delivery charges: ")
-    discount_applied = self.getInputFromUser("Enter discount applied: ")
-    platform_fee = self.getInputFromUser("Enter Extra Charges: ")
-    tax_applied = (15/100).to_f
-    subtotal = ((order_price.to_f + standard_delivery.to_f + platform_fee.to_f)-discount_applied.to_f)
-    subtotal += tax_applied
+    order_price = self.getInputFromUser("Enter Order Price: ").to_f
+    standard_delivery = self.getInputFromUser("Enter delivery charges: ").to_f
+    discount_applied = self.getInputFromUser("Enter discount applied: ").to_f
+    platform_fee = self.getInputFromUser("Enter Extra Charges: ").to_f
+    tax_applied = (order_price.to_f * (15/100).to_f)
+    subtotal = ((order_price.to_f + standard_delivery.to_f + platform_fee.to_f + tax_applied.to_f)-discount_applied.to_f)
     return subtotal.to_f
   end
 
@@ -24,7 +23,7 @@ module Functions
     getBillPrice = self.generateBill().to_f
     payment = self.getInputFromUser("Payment Please: ").to_f
     change = (payment - getBillPrice)
-    return { "Payment" => payment, "Your Change: " => change, "Subtotal" => getBillPrice}
+    return { "Payment" => payment, "Change: " => change, "Subtotal" => getBillPrice}
   end
 end
 
