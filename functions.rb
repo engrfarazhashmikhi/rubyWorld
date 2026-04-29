2dd0c04c4082b6d194872cd535f39dc12c9337a0
engrfarazhashmikhi <farazhashmi90000@gmail.com>
2026-04-29 11:43:25 +0000

    Vlues rounded for easy OUTPUT

--- functions.rb
+++ functions.rb
@@ -9,21 +9,21 @@ module Functions
     return statement
   end
 
-  def self.generateBill
+  def self.generateBill()
     order_price = self.getInputFromUser("Enter Order Price: ").to_f
     standard_delivery = self.getInputFromUser("Enter delivery charges: ").to_f
     discount_applied = self.getInputFromUser("Enter discount applied: ").to_f
     platform_fee = self.getInputFromUser("Enter Extra Charges: ").to_f
-    tax_applied = (order_price.to_f * (15/100).to_f)
-    subtotal = ((order_price.to_f + standard_delivery.to_f + platform_fee.to_f + tax_applied.to_f)-discount_applied.to_f)
+    tax_applied = (order_price * (15/100)).to_f
+    subtotal = ((order_price + tax_applied + standard_delivery + platform_fee ) - discount_applied)
     return subtotal.to_f
   end
 
   def self.checkOut()
-    getBillPrice = self.generateBill().to_f
-    payment = self.getInputFromUser("Payment Please: ").to_f
-    change = (payment - getBillPrice)
-    return { "Payment" => payment, "Change: " => change, "Subtotal" => getBillPrice}
+    getBillPrice = (self.generateBill().to_f.round(2))
+    payment = (self.getInputFromUser("Payment Please: ").to_f.round(2))
+    change = ((payment - getBillPrice).to_f.round(2))
+    return { :Payment  => payment, :Change  => change, :Subtotal => getBillPrice }
   end
 end
 
